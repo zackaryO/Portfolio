85ade88bb220741a62928a94c66897cec1084f5a
Zack Otterstrom <90364473+zackaryO@users.noreply.github.com>
2023-11-26 22:27:36 -0700

    update

--- Django/myapp/models.py
+++ Django/myapp/models.py
@@ -47,6 +47,9 @@ class Skill(models.Model):
     proficiency_level = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class Resume(models.Model):
     name = models.CharField(max_length=200)
